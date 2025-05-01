11db979944e0e6f17982555aeb92b2b3f8cffa89
Thisguy12378 <collincm65@outlook.com>
2025-05-01 19:42:02 -0400

    Drag Added, no visuals

--- 421FinalProj/Form1.cs
+++ 421FinalProj/Form1.cs
@@ -11,7 +11,7 @@ namespace _421FinalProj
             // Handle DragEnter to validate the dragged object
             Canvas.DragEnter += (s, e) =>
             {
-                if (e.Data.GetDataPresent(typeof(TaskCard)))
+                if (e.Data?.GetDataPresent(typeof(TaskCard)) == true)
                 {
                     e.Effect = DragDropEffects.Move;
                 }
@@ -24,7 +24,7 @@ namespace _421FinalProj
             // Handle DragDrop to place the TaskCard on the canvas
             Canvas.DragDrop += (s, e) =>
             {
-                if (e.Data.GetData(typeof(TaskCard)) is TaskCard taskCard)
+                if (e.Data?.GetData(typeof(TaskCard)) is TaskCard taskCard)
                 {
                     // Convert the drop location to canvas coordinates
                     var dropLocation = Canvas.PointToClient(new Point(e.X, e.Y));
@@ -41,23 +41,77 @@ namespace _421FinalProj
         private void Form1_Load(object? sender, EventArgs e)
         {
             // put startup logic here
-            flowTasks.Controls.Add(new PaletteItem("Email", "EmailTask"));
-            flowTasks.Controls.Add(new PaletteItem("SMS", "SmsTask"));
+            //flowTasks.Controls.Add(new PaletteItem("Email", "EmailTask"));
+            //flowTasks.Controls.Add(new PaletteItem("SMS", "SmsTask"));
+
+            var emailBox = new Label
+            {
+                Text = "Email",
+                BackColor = Color.LightBlue,
+                AutoSize = true,
+                Padding = new Padding(10),
+                BorderStyle = BorderStyle.FixedSingle
+            };
+            emailBox.MouseMove += EmailBox_MouseMove;
+            flowTasks.Controls.Add(emailBox);
+
+            var smsBox = new Label
+            {
+                Text = "SMS",
+                BackColor = Color.LightGreen,
+                AutoSize = true,
+                Padding = new Padding(10),
+                BorderStyle = BorderStyle.FixedSingle
+            };
+            smsBox.MouseMove += SmsBox_MouseMove;
+            flowTasks.Controls.Add(smsBox);
+
+            Canvas.DragEnter += BoxdragEnter;
+            Canvas.DragDrop += BoxDrop;
         }
-        //void Canvas_DragEnter(object? s, DragEventArgs e)
-        //{
-        //    if (e.Data!.GetDataPresent(DataFormats.Text))
-        //        e.Effect = DragDropEffects.Copy;
-        //}
 
-        //void Canvas_DragDrop(object? s, DragEventArgs e)
-        //{
-        //    string? type = e.Data!.GetData(DataFormats.Text) as string;
-        //    if (type == null) return;
+        private void EmailBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                if (sender is Control emailBox)
+                {
+                    // Fix for CS1503: Use a string key instead of typeof(TaskCard)
+                    emailBox.DoDragDrop(emailBox.Text, DragDropEffects.Copy | DragDropEffects.Move);
+                }
+            }
+        }
 
-        //    var pt = Canvas.PointToClient(new Point(e.X, e.Y));
-        //    var card = new TaskCard(type) { Location = pt };
-        //    Canvas.Controls.Add(card);
-        //}
+        private void SmsBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                if (sender is Control smsBox)
+                {
+                    smsBox.DoDragDrop(smsBox.Text, DragDropEffects.Copy | DragDropEffects.Move);
+                }
+            }
+        }
+
+        private void BoxdragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.StringFormat))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+
+        }
+
+        private void BoxDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.StringFormat))
+            {
+                string droppedData = (string)e.Data.GetData(DataFormats.StringFormat);
+            }
+        }
     }
 }
