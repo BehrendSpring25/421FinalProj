d07a3d8a8a46fd7c9a4f148dbbc6d383da5c5e0a
Thisguy12378 <collincm65@outlook.com>
2025-05-02 17:03:28 -0400

    Scheduler Done

--- 421FinalProj/Scheduler.cs
+++ 421FinalProj/Scheduler.cs
@@ -1,12 +1,51 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace _421FinalProj
 {
     internal class Scheduler
     {
+        private Thread? runningThread;
+        private readonly object threadLock = new object();
+        private readonly Queue<Thread> scheduled = new Queue<Thread>(); 
+
+        public void Enter()
+        {
+            Thread thisThread = Thread.CurrentThread;
+
+            lock (threadLock)
+            {
+                if (runningThread == null)
+                {
+                    runningThread = thisThread;
+                    return;
+                }
+
+                scheduled.Enqueue(thisThread);
+
+                while (runningThread != thisThread)
+                {
+                    Monitor.Wait(threadLock); 
+                }
+            }
+        }
+
+        public void Exit()
+        {
+            lock (threadLock)
+            {
+                if (scheduled.Count > 0)
+                {
+                    runningThread = scheduled.Dequeue();
+                    Monitor.PulseAll(threadLock); 
+                }
+                else
+                {
+                    runningThread = null;
+                }
+            }
+        }
     }
 }
